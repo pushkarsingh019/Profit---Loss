7e09b1669697d6158fb76094950dfae1182c74fe
Pushkar Singh <stoazest@gmail.com>
2022-01-18 22:57:55 +0530

    Added Only 2 Decimal Places

--- index.js
+++ index.js
@@ -6,14 +6,21 @@ var output = document.querySelector("#output");
 
 // Functions
 
+function validate(){
+    // to validate that all data is filled in
+}
+
 function check(profit, percentage){
     if(profit > 1){
+        output.style.color = "green";
         return "Yayy! your profit is   $" + profit + " and profit percentage is %" + percentage + " :)";
     }
     else if (profit < 0) {
+        output.style.color = "red";
         return "Whoops!! Your loss is  $" + profit + " and loss percentage is %" + percentage + " :(" ;
     }
     else if (profit == 0){
+        output.style.color = "grey";
         return "I dont what to say, your net worth is the same as before. Blessing in disguise?"
     }
     else {
@@ -27,9 +34,10 @@ function calculateProfit(){
    var currentPriceA = Number(currentPrice.value);
 
    let absoluteProfit = (currentPriceA * stocksNumberA) - (initalPriceA * stocksNumberA);
+   absoluteProfit = Number.parseFloat(absoluteProfit).toFixed(2);
 
    let percentageProfit = (absoluteProfit/initalPriceA) * 100;
-   
+   percentageProfit = Number.parseFloat(percentageProfit).toFixed(2);
 
    var message = check(absoluteProfit, percentageProfit);
 
